091c72e1d56c6cfc3e868a7991bf51d0768467f6
SebastianVillaH <sebastian.villa195@pascualbravo.edu.co>
2026-08-31 13:06:49 -0500

    Update app_nivel_cornare.py

--- app_nivel_cornare.py
+++ app_nivel_cornare.py
@@ -83,7 +83,7 @@ def detectar_coordenadas(datos_json):
             pass
     return LAT_DEFECTO, LON_DEFECTO, False
 
-st.image("Sensor.webp", caption="Sensor")
+st.image("Sensor.webp", caption="Sensor",width = 250, height= 250)
 
 
 def calcular_indice_calidad(df):
